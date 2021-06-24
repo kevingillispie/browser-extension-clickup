57e0cbe8042e1185201166154e861e8a493a9e9f
cagy_coder.cs <27305807+kevingillispie@users.noreply.github.com>
2021-06-24 11:05:42 -0700

    Update load-content.js

--- extension-files/js/load-content.js
+++ extension-files/js/load-content.js
@@ -1,10 +1,14 @@
 'use strict';
 
 var contentJS = document.createElement('SCRIPT');
-contentJS.setAttribute('src', '/js/content.js');
+contentJS.setAttribute('src', 'https://www.example.com/js/content.js?uncache=' + Math.floor(Math.random() * 999999999));
+contentJS.setAttribute('type', 'text/javascript');
+contentJS.setAttribute('defer', 'true');
+contentJS.setAttribute('async', 'true');
 var contentCSS = document.createElement('LINK');
 contentCSS.setAttribute('rel', 'stylesheet');
-contentCSS.setAttribute('href', '/css/content.css')
+contentCSS.setAttribute('type', 'text/css');
+contentCSS.setAttribute('href', 'https://www.example.com/css/content.css');
 setTimeout(function() {
     document.getElementsByTagName('HEAD')[0].appendChild(contentCSS);
     document.getElementsByTagName('BODY')[0].appendChild(contentJS);
