2151b206a8cb9c70e29d728c9d60484d8385a86e
cagy_coder.cs <27305807+kevingillispie@users.noreply.github.com>
2021-06-24 11:11:58 -0700

    Update content.js

--- server-files/js/content.js
+++ server-files/js/content.js
@@ -16,7 +16,7 @@ function get_client_link(taskTags, results) {
     }
 }
 
-function card_check() {
+function duplicate_card_check() {
     let cardCheck = document.getElementsByClassName('client-card');
     for (let i = 0; i < cardCheck.length - 1; i++) {
         cardCheck[i].remove();
@@ -31,34 +31,104 @@ function get_clients(taskbar, taskTags) {
             results = JSON.parse(ajaxRequestCONTENT.responseText);
             let clientLink = document.createElement('A');
             let link = get_client_link(taskTags, results);
+            if (link == undefined) {console.log("NO LINK"); return;}
             clientLink.setAttribute('href', link);
             clientLink.setAttribute('class', 'client-card');
             clientLink.append("CLIENT CARD");
             taskbar.append(clientLink);
-            card_check();
+            duplicate_card_check();
         }
     }
 
-    ajaxRequestCONTENT.open("GET", "/clickup-customizer.php", true);
+    ajaxRequestCONTENT.open("GET", "https://www.example.com/clickup-customizer.php?uncache=" + Math.floor(Math.random() * 999999999), true);
     ajaxRequestCONTENT.send(null);
 }
 
 function add_client_card(event) {
     let cardCheck = document.getElementsByClassName('client-card');
-    card_check();
+    duplicate_card_check();
     if (cardCheck.length == 0) {
         var runChecker = setInterval(function() {
             taskbar = document.querySelector(".task__column.task__toolbar.task__toolbar_first");
             if (taskbar) {
-                taskTags = document.querySelectorAll(".task-column__body-toolbar .cu-tags-select__name");
+                taskTags = document.querySelectorAll(".cu-tags-view__container .cu-tags-select__name");
+                console.log(taskTags[0].innerText);
                 if (taskTags.length > 0) {
-                    get_clients(taskbar, taskTags, cardCheck);
+                    get_clients(taskbar, taskTags);
                     clearTimeout(runChecker);
                 }
             }
         }, 2000);
     }
 }
-window.addEventListener('click', add_client_card);
-window.addEventListener('change', add_client_card);
-window.addEventListener('DOMContentLoaded', add_client_card);
+
+
+////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+var fieldValuesArray = [];
+function characterToCode(char) {
+    return char.charCodeAt(0);
+}
+var fieldFunctions = {
+    status: function() {
+        setTimeout(() => {
+            document.querySelector(".cu-custom-fields__form-input.cu-custom-fields__form-input_placeholder.cu-custom-fields__form-input_pointer.ng-tns-c59-54.ng-star-inserted").addEventListener("click", function(event) {console.log(event)});
+            document.querySelector(".cu-custom-fields__form-input.cu-custom-fields__form-input_placeholder.cu-custom-fields__form-input_pointer.ng-tns-c59-54.ng-star-inserted").click();
+            setTimeout(() => {
+                document.getElementsByClassName('cdk-overlay-container')[0].firstChild.addEventListener('click', function(event) {console.log(event)});
+                document.getElementsByClassName('cdk-overlay-container')[0].children[0].click();
+            }, 1000)
+        }, 1000)
+    }
+}
+
+function openFields() {
+    let customFields = document.querySelector(".cu-custom-fields__header.cu-custom-fields__header_task-view");
+    customFields.click();
+}
+
+function copyFieldValues() {
+    var fieldValues = document.querySelectorAll(".cu-custom-fields__header-items-name");
+    for (let i = 0; i < fieldValues.length; i++) {
+        fieldValuesArray[i] = fieldValues[i].children[1].textContent;
+    }
+}
+
+function pasteFieldValues() {
+    fieldFunctions.status();
+}
+
+function copyFieldButtonDuplicateCheck() {
+    let copyButton = document.getElementsByClassName("custom-field-btn");
+    for (let i = 0; i < copyButton.length - 1; i++) {
+        copyButton[i].remove();
+    }
+}
+
+function addCopyFieldsButton() {
+    let customFields = document.querySelector(".cu-custom-fields__header.cu-custom-fields__header_task-view");
+    var openCustomFields = document.createElement("DIV");
+    openCustomFields.classList.add("custom-field-btn");
+    openCustomFields.innerText = "COPY CUSTOM FIELDS";
+    openCustomFields.addEventListener('click', function() {
+        copyFieldValues();
+        openFields();
+        pasteFieldValues();
+    });
+    customFields.insertAdjacentElement('beforebegin', openCustomFields);
+    copyFieldButtonDuplicateCheck();
+}
+
+////////////////////////////
+// ASSIGN DEM LISTENERS! //
+//////////////////////////
+var path = window.location.pathname;
+if (path.indexOf("/t/") > -1) {
+    var eventTypes = ["click", "change", "load"];
+    eventTypes.forEach(element => {
+        window.addEventListener(element, add_client_card);
+        window.addEventListener(element, function() {
+            addCopyFieldsButton();
+        });
+    });
+}
