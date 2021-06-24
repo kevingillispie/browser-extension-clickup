bf3166aa87b5f3424657c2bfcbee481b4db94f27
cagy_coder.cs <27305807+kevingillispie@users.noreply.github.com>
2021-06-24 12:04:46 -0700

    Update and rename extension-files/js/load-content.js to BROWSER/js/load-content.js

--- BROWSER/js/load-content.js
+++ BROWSER/js/load-content.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var contentJS = document.createElement('SCRIPT');
-contentJS.setAttribute('src', 'https://www.example.com/js/content.js?uncache=' + Math.floor(Math.random() * 999999999));
+contentJS.setAttribute('src', 'https://www.example.com/SERVER/js/content.js?uncache=' + Math.floor(Math.random() * 999999999));
 contentJS.setAttribute('type', 'text/javascript');
 contentJS.setAttribute('defer', 'true');
 contentJS.setAttribute('async', 'true');
 var contentCSS = document.createElement('LINK');
 contentCSS.setAttribute('rel', 'stylesheet');
 contentCSS.setAttribute('type', 'text/css');
-contentCSS.setAttribute('href', 'https://www.example.com/css/content.css');
+contentCSS.setAttribute('href', 'https://www.example.com/SERVER/css/content.css');
 setTimeout(function() {
     document.getElementsByTagName('HEAD')[0].appendChild(contentCSS);
     document.getElementsByTagName('BODY')[0].appendChild(contentJS);
